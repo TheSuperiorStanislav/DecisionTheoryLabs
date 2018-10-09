70c81d115ecdefd3c37396c2823eec14ff64e259
thssuperiorstanislav <stasontos2@hotmail.com>
2018-10-09 18:21:57 +0700

    add UseCase lecture1

---
+++ app/src/main/java/com/study/thesuperiorstanislav/decisiontheorylabs/lecture1/domain/usecase/CacheDataLecture1.kt
@@ -0,0 +1,35 @@
+package com.study.thesuperiorstanislav.decisiontheorylabs.lecture1.domain.usecase
+
+import com.study.thesuperiorstanislav.decisiontheorylabs.UseCase
+import com.study.thesuperiorstanislav.decisiontheorylabs.data.source.Lecture1DataSource
+import com.study.thesuperiorstanislav.decisiontheorylabs.lab1.domain.model.Point
+
+class CacheDataLecture1 (private val lecture1Repository: Lecture1DataSource): UseCase<CacheDataLecture1.RequestValues, CacheDataLecture1.ResponseValue>() {
+
+    override fun executeUseCase(requestValues: RequestValues?) {
+        if (requestValues != null) {
+            val function = requestValues.function
+            val pointList = requestValues.pointList
+            val alpha = requestValues.alpha
+            val value = requestValues.value
+
+            lecture1Repository.cacheData(function,
+                    pointList,
+                    alpha,
+                    value, object : Lecture1DataSource.CacheDataCallback {
+                override fun onSaved() {
+                    val responseValue = ResponseValue()
+                    useCaseCallback?.onSuccess(responseValue)
+                }
+            })
+        }
+    }
+
+
+    class RequestValues(val function: String,
+                        val pointList: MutableList<Point>,
+                        val alpha: Double,
+                        val value: Double) : UseCase.RequestValues
+
+    class ResponseValue : UseCase.ResponseValue
+}
---
+++ app/src/main/java/com/study/thesuperiorstanislav/decisiontheorylabs/lecture1/domain/usecase/DoTheThingLecture1.kt
@@ -0,0 +1,48 @@
+package com.study.thesuperiorstanislav.decisiontheorylabs.lecture1.domain.usecase
+
+import com.study.thesuperiorstanislav.decisiontheorylabs.UseCase
+import net.objecthunter.exp4j.ExpressionBuilder
+
+
+class DoTheThingLecture1: UseCase<DoTheThingLecture1.RequestValues, DoTheThingLecture1.ResponseValue>() {
+
+    override fun executeUseCase(requestValues: RequestValues?) {
+        if (requestValues != null) {
+            try {
+                val function = requestValues.function
+                val u = requestValues.u
+                var alpha = requestValues.alpha
+                val value = requestValues.value
+
+                val fU = calculateFunc(u, function)
+                val uNew = u + alpha * (value - fU)
+                val fUNew = calculateFunc(uNew, function)
+
+                if (Math.abs(value - fU) < Math.abs(value - fUNew))
+                    alpha = -alpha
+
+                val responseValue = ResponseValue(alpha,uNew,fUNew)
+                useCaseCallback?.onSuccess(responseValue)
+            } catch (e: Exception) {
+                useCaseCallback?.onError(UseCase.Error(Error.UNKNOWN_ERROR, e.localizedMessage))
+            }
+        }
+    }
+
+    private fun calculateFunc(u: Double, function: String): Double {
+        val exp = ExpressionBuilder(function)
+                .variables("u")
+                .build()
+                .setVariable("u", u)
+        return exp.evaluate()
+    }
+
+    class RequestValues(val function: String,
+                        val u: Double,
+                        val alpha: Double,
+                        val value: Double) : UseCase.RequestValues
+
+    class ResponseValue(val alpha: Double,
+                        val uNew: Double,
+                        val fUNew:Double) : UseCase.ResponseValue
+}
