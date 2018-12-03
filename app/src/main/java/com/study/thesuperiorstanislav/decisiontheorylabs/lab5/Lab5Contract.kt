f64e6ae9266f0c60c7d604e93c57fa7d1e763def
TheSuperiorStanislav <stasontos2@hotmail.com>
2018-12-03 15:07:24 +0700

    make lab5 contract and presenter

--- app/src/main/java/com/study/thesuperiorstanislav/decisiontheorylabs/lab5/Lab5Contract.kt
+++ app/src/main/java/com/study/thesuperiorstanislav/decisiontheorylabs/lab5/Lab5Contract.kt
@@ -1,4 +1,29 @@
 package com.study.thesuperiorstanislav.decisiontheorylabs.lab5
 
+import com.study.thesuperiorstanislav.decisiontheorylabs.BasePresenter
+import com.study.thesuperiorstanislav.decisiontheorylabs.BaseView
+import com.study.thesuperiorstanislav.decisiontheorylabs.UseCase
+import com.study.thesuperiorstanislav.decisiontheorylabs.lab1.domain.model.Point
+
 interface Lab5Contract {
+    interface View : BaseView<Presenter> {
+
+        var isActive: Boolean
+
+        fun drawGraph(pointListOriginal: List<Point>,
+                      pointListRestored: List<Point>)
+
+        fun onError(error: UseCase.Error)
+
+        fun onLoadingError(error: UseCase.Error)
+
+    }
+
+    interface Presenter : BasePresenter {
+
+        fun getData()
+
+        fun doTheThing(n: Int, delta: Double, function: String, alpha: Double)
+
+    }
 }
--- app/src/main/java/com/study/thesuperiorstanislav/decisiontheorylabs/lab5/Lab5Presenter.kt
+++ app/src/main/java/com/study/thesuperiorstanislav/decisiontheorylabs/lab5/Lab5Presenter.kt
@@ -1,5 +1,67 @@
 package com.study.thesuperiorstanislav.decisiontheorylabs.lab5
 
-class Lab5Presenter{
+import com.study.thesuperiorstanislav.decisiontheorylabs.UseCase
+import com.study.thesuperiorstanislav.decisiontheorylabs.UseCaseHandler
+import com.study.thesuperiorstanislav.decisiontheorylabs.lab5.domain.DoTheThingLab5
+import com.study.thesuperiorstanislav.decisiontheorylabs.lab5.domain.GetDataLab5
+
+class Lab5Presenter(private val lab5View: Lab5Contract.View,
+                    private val doTheThingLab5: DoTheThingLab5,
+                    private val getDataLab5: GetDataLab5):Lab5Contract.Presenter{
+
+    override fun start() {
+        lab5View.isActive = true
+        getData()
+    }
+    
+    override fun getData() {
+        val requestValue = GetDataLab5.RequestValues()
+        UseCaseHandler.execute(getDataLab5, requestValue,
+                object : UseCase.UseCaseCallback<GetDataLab5.ResponseValue> {
+                    override fun onSuccess(response: GetDataLab5.ResponseValue) {
+                        // The lab5View may not be able to handle UI updates anymore
+                        if (!lab5View.isActive) {
+                            return
+                        }
+
+                        lab5View.drawGraph(response.pointListOriginal,
+                                response.pointListRestored)
+                    }
+
+                    override fun onError(error: UseCase.Error) {
+                        // The lab5View may not be able to handle UI updates anymore
+                        if (!lab5View.isActive) {
+                            return
+                        }
+
+                        lab5View.onError(error)
+                    }
+                })
+    }
+
+    override fun doTheThing(n: Int, delta: Double, function: String, alpha: Double) {
+        val requestValue = DoTheThingLab5.RequestValues(n, delta, function, alpha)
+        UseCaseHandler.execute(doTheThingLab5, requestValue,
+                object : UseCase.UseCaseCallback<DoTheThingLab5.ResponseValue> {
+                    override fun onSuccess(response: DoTheThingLab5.ResponseValue) {
+                        // The lab5View may not be able to handle UI updates anymore
+                        if (!lab5View.isActive) {
+                            return
+                        }
+
+                        lab5View.drawGraph(response.pointListOriginal,
+                                response.pointListRestored)
+                    }
+
+                    override fun onError(error: UseCase.Error) {
+                        // The lab5View may not be able to handle UI updates anymore
+                        if (!lab5View.isActive) {
+                            return
+                        }
+
+                        lab5View.onError(error)
+                    }
+                })
+    }
 
 }
